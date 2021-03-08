82c97647c2df2e965f601f00ba90e97407ed7b0a
Blake Runyon <blakerunyon09@gmail.com>
2021-03-08 16:07:03 -0700

    Predictive Search to Span

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Cha.")
+const backendBaseAPI = "http://localhost:3000/search?name_id="
+const coinSearch = document.querySelector('#coin_search')
+const searchSuggestion = document.querySelector('#search_suggestion')
+
+const handleResponse = (response) => {
+  return response.json()
+}
+
+coinSearch.addEventListener('keyup', (event) => {
+  input = event.target.value
+  searchAPI = backendBaseAPI + input
+  if(event.target.value != ""){
+    fetch(searchAPI)
+    .then(handleResponse)
+    .then(data => {
+      searchSuggestion.textContent = data.name_id
+      console.log(event)
+    })
+  }
+  else{
+    searchSuggestion.textContent = ""
+  }
+})
