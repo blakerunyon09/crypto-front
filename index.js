a815bbd0e2c0c5e2af950cac98a229e7646873b2
Blake Runyon <blakerunyon09@gmail.com>
2021-03-09 11:36:06 -0700

    Added Filters

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const coinGeckoAPI = "https://api.coingecko.com/api/v3/coins/list"
 const coinSearch = document.querySelector('#coin_search')
 const searchSuggestion = document.querySelector('#search_suggestion')
 const searchInput = document.querySelector('#search_input')
+const priceButton = document.querySelector('#price')
+const marCapButton = document.querySelector('#mar_cap')
+const chart = document.querySelector('#myChart')
 const priceArray = []
+const marketArray = []
 const timeArray = []
-let date = new Date()
 
-// SEARCH & BUILD CHART
+priceButton.addEventListener('click', function(){
+  fetch(`https://api.coingecko.com/api/v3/coins/${input.toLowerCase()}/market_chart?vs_currency=usd&days=365&interval=daily`)
+  .then(handleResponse)
+  .then(data => {
+    marketArray.length = 0
+    createChartData(data, priceArray, "prices")
+    renderChart(timeArray,priceArray, marketArray)
+  })
+})
+
+marCapButton.addEventListener('click', function(){
+  fetch(`https://api.coingecko.com/api/v3/coins/${input.toLowerCase()}/market_chart?vs_currency=usd&days=365&interval=daily`)
+  .then(handleResponse)
+  .then(data => {
+    priceArray.length = 0
+    createChartData(data, priceArray, "market_caps")
+    renderChart(timeArray,priceArray, marketArray)
+  })
+})
+
+// PREDICTS USERS SEARCH
 const predictSearch = (event) => {
   searchSuggestion.textContent = ""
   input = event.target.value.toUpperCase()
@@ -24,6 +47,7 @@ const predictSearch = (event) => {
   }
 }
 
+// EXECUTES SEARCH
 const handleSearch = (e) => {
    if(e.key == "Enter"){
     priceArray.length = 0
@@ -31,8 +55,9 @@ const handleSearch = (e) => {
     fetch(`https://api.coingecko.com/api/v3/coins/${input.toLowerCase()}/market_chart?vs_currency=usd&days=365&interval=daily`)
       .then(handleResponse)
       .then(data => {
-        createChartData(data, priceArray)
-        renderChart(timeArray, priceArray)
+        createChartData(data, priceArray, "prices")
+        createChartData(data, marketArray, "market_caps")
+        renderChart(timeArray,priceArray, marketArray)
       })
    }
 }
@@ -47,22 +72,36 @@ const handleResponse = (response) => {
 }
 
 // CREATES CHART DATA
-const createChartData = (data, array) => {
-  data.prices.forEach(timeframe => {
-    array.push(timeframe[1])
-    date.setDate(date.getDate() - 1)
-    timeArray.push(date.toString().slice(0, -24))
-  })
+const createChartData = (data, array, capitalType) => {
+  if(capitalType == "prices"){
+    let date = new Date()
+    timeArray.length = 0
+    data.prices.forEach(timeframe => {
+      array.push(timeframe[1])
+      date.setDate(date.getDate() - 1)
+      timeArray.push(date.toString().slice(0, -24))
+    })
+  }
+  else if(capitalType == "market_caps"){
+    let date = new Date()
+    timeArray.length = 0
+    data.market_caps.forEach(timeframe => {
+      array.push(timeframe[1])
+      date.setDate(date.getDate() - 1)
+      timeArray.push(date.toString().slice(0, -24))
+    })
+  }
 }
 
 // CREATES CHART
-const renderChart = (timeArray, priceArray) => {
+const renderChart = (timeArray, priceArray, marketArray) => {
   var ctx = document.getElementById('myChart').getContext('2d');
             var myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
                     labels: timeArray.reverse(),
-                    datasets: [{
+                    datasets: [
+                      {
                         label: '',
                         data: priceArray,
                         backgroundColor: [
@@ -82,7 +121,29 @@ const renderChart = (timeArray, priceArray) => {
                             'rgba(255, 159, 64, 1)'
                         ],
                         borderWidth: 1
-                    }]
+                    },
+                    {
+                      label: '',
+                      data: marketArray,
+                      backgroundColor: [
+                          'rgba(255, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(255, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(153, 102, 255, 0.2)',
+                          'rgba(255, 159, 64, 0.2)'
+                      ],
+                      borderColor: [
+                          'rgba(255, 99, 132, 1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)'
+                      ],
+                      borderWidth: 1
+                  }
+                  ]
                 },
                 options: {
                     scales: {
