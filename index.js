fda95f711409aa3d18959e3eb186ca872bca892b
Blake Runyon <blakerunyon09@gmail.com>
2021-03-10 10:28:49 -0700

    Updates

--- index.js
+++ index.js
@@ -172,54 +172,49 @@ const renderChart = (timeArray, priceArray, marketArray) => {
                         label: '',
                         data: priceArray,
                         backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
+                            'rgba(255, 99, 132, 0.0)'
                         ],
                         borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
+                            '#A3BE8C'
+                        ]
                     },
                     {
                       label: '',
                       data: marketArray,
                       backgroundColor: [
-                          'rgba(255, 99, 132, 0.2)',
-                          'rgba(54, 162, 235, 0.2)',
-                          'rgba(255, 206, 86, 0.2)',
-                          'rgba(75, 192, 192, 0.2)',
-                          'rgba(153, 102, 255, 0.2)',
-                          'rgba(255, 159, 64, 0.2)'
+                          'rgba(255, 99, 132, 0.0)'
                       ],
                       borderColor: [
-                          'rgba(255, 99, 132, 1)',
-                          'rgba(54, 162, 235, 1)',
-                          'rgba(255, 206, 86, 1)',
-                          'rgba(75, 192, 192, 1)',
-                          'rgba(153, 102, 255, 1)',
-                          'rgba(255, 159, 64, 1)'
-                      ],
-                      borderWidth: 1
+                          '#A3BE8C'
+                      ]
                   }
                   ]
                 },
                 options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero: true
-                            }
-                        }]
+                  elements: {
+                    line: {
+                      borderJoinStyle: 'miter',
+                      borderWidth: 2
                     }
+                  },
+                  scales: {
+                    xAxes: [{
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0)"
+                        },
+                        ticks: {
+                          display: false
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0)"
+                        },
+                        ticks: {
+                          display: false
+                        }
+                    }]
+                  }
                 }
             }
             )
